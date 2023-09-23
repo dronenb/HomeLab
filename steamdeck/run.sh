3abd8eac089adb884f8bece44207e438ce607a48
dronenb <benjamin.dronen@gmail.com>
2023-09-23 16:44:41 -0400

    Adding Steam Deck

--- steamdeck/run.sh
+++ steamdeck/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# shellcheck disable=SC1091
+set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# shellcheck disable=SC1091
 source "${SCRIPT_DIR}/../bash/bitwarden_env.sh"
 cd ansible || exit 1
 ansible-galaxy install -r requirements.yaml
