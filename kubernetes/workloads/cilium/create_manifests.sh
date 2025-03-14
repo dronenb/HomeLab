6b19bd6bf97a828dff918efe72b82d352609f54e
Ben Dronen <dronenb@users.noreply.github.com>
2025-03-14 16:09:26 -0700

    chore(cilium): upgrade to 1.17.1 (again) and disable externalTrafficPolicy: Local on service type LB's
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- kubernetes/workloads/cilium/create_manifests.sh
+++ kubernetes/workloads/cilium/create_manifests.sh
@@ -8,42 +8,44 @@ shopt -s failglob
 mkdir -p manifests/base
 pushd manifests/base > /dev/null || exit 1
 
-export CILIUM_VERSION="1.16.5"
+export CILIUM_VERSION="1.17.1"
 export NAMESPACE=cilium
 
 helm repo update
 # https://docs.cilium.io/en/stable/helm-reference/
+# envoy.enabled won't work with SELinux enabled: https://docs.cilium.io/en/latest/security/network/proxy/envoy/#known-limitations
+# externalTrafficPolicy=Local doesn't work with kube-vip load balancers because of the fake endpointslices cilium uses
 helm template cilium cilium/cilium \
     --api-versions gateway.networking.k8s.io/v1/GatewayClass \
     --include-crds \
     --version "${CILIUM_VERSION}" \
     --namespace "${NAMESPACE}" \
-    --set routingMode=tunnel \
-    --set tunnelProtocol=geneve \
-    --set kubeProxyReplacement=true \
-    --set k8sServiceHost=patchme \
-    --set k8sServicePort=6443 \
-    --set loadBalancer.mode=dsr \
-    --set loadBalancer.dsrDispatch=geneve \
-    --set l2announcements.enabled=true \
-    --set hubble.relay.enabled=true \
-    --set ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16" \
-    --set hubble.ui.enabled=true \
+    --set defaultLBServiceIPAM=none \
+    --set envoy.enabled=false \
     --set gatewayAPI.enabled=true \
-    --set gatewayAPI.externalTrafficPolicy=Local \
+    --set gatewayAPI.externalTrafficPolicy=Cluster \
+    --set hubble.relay.enabled=true \
+    --set hubble.ui.enabled=true \
+    --set hubble.tls.auto.certValidityDuration=1095 \
+    --set hubble.tls.auto.enabled=true \
+    --set hubble.tls.auto.method=cronJob \
+    --set hubble.tls.auto.schedule="0 0 1 */4 *" \
     --set ingressController.enabled=true \
     --set ingressController.loadbalancerMode=shared \
     --set ingressController.service.loadBalancerClass=kube-vip.io/kube-vip-class \
     --set ingressController.service.allocateLoadBalancerNodePorts=false \
-    --set ingressController.service.externalTrafficPolicy=Local \
+    --set ingressController.service.externalTrafficPolicy=Cluster \
+    --set ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16" \
+    --set k8sServiceHost=patchme \
+    --set k8sServicePort=6443 \
+    --set kubeProxyReplacement=true \
+    --set l2announcements.enabled=true \
     --set l7Proxy=true \
+    --set loadBalancer.dsrDispatch=geneve \
     --set loadBalancer.l7.backend=envoy \
-    --set hubble.tls.auto.enabled=true \
-    --set hubble.tls.auto.method=cronJob \
-    --set hubble.tls.auto.certValidityDuration=1095 \
-    --set hubble.tls.auto.schedule="0 0 1 */4 *" \
-    --set envoy.enabled=false | \
-    # envoy.enabled won't work with SELinux enabled: https://docs.cilium.io/en/latest/security/network/proxy/envoy/#known-limitations
+    --set loadBalancer.mode=dsr \
+    --set routingMode=tunnel \
+    --set tunnelProtocol=geneve | \
     yq --no-colors --prettyPrint '... comments=""' | \
     kubectl-slice -o . --template "{{ .kind | lower }}.yaml"
 
