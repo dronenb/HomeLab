36832b094a3425c9966b45e3dd3f14e558108c2c
dronenb <dronenb@users.noreply.github.com>
2025-03-16 15:45:10 -0700

    feat(percona-postgresql): incept (#92)
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

---
+++ kubernetes/workloads/percona-postgresql/create_manifests.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+shopt -s failglob
+
+# helm search repo percona/pg-operator
+VERSION=2.5.1
+NAMESPACE=percona-postgresql
+
+helm repo add percona https://percona.github.io/percona-helm-charts/
+helm repo update
+
+mkdir -p manifests/base
+pushd manifests/base > /dev/null || exit 1
+
+echo "---" >> namespace.yaml
+kubectl create namespace "${NAMESPACE}" -o yaml --dry-run=client | \
+    kubectl neat \
+    >> namespace.yaml
+
+helm template percona/pg-operator \
+    --version "${VERSION}" \
+    --namespace "${NAMESPACE}" \
+    --include-crds \
+    --set "fullNameOverride=${NAMESPACE}" | \
+    yq --no-colors --prettyPrint '... comments=""' | \
+    kubectl-slice -o . --template "{{ .kind | lower }}.yaml"
+
+# Iterate over each yaml file
+files=()
+for file in *.yaml; do
+    if [[ "${file}" == "kustomization.yaml" ]]; then
+        continue
+    fi
+    files+=("${file}")
+    contents="$(cat "${file}")"
+    printf -- "---\n# yamllint disable rule:line-length\n%s" "${contents}" > "${file}"
+done
+
+# Create kustomize file
+cat <<EOF > kustomization.yaml
+---
+kind: Kustomization
+apiVersion: kustomize.config.k8s.io/v1beta1
+resources:
+$(printf "  - %s\n" "${files[@]}")
+EOF
+
+# Format YAML
+prettier . --write
+popd > /dev/null || exit 1
