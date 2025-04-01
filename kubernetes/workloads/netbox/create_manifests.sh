cfd1e7ce750f60b41bd6a3104d0cf5ec558a732a
dronenb <dronenb@users.noreply.github.com>
2025-04-01 18:47:00 -0400

    feat(netbox): incept (#98)
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

---
+++ kubernetes/workloads/netbox/create_manifests.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+shopt -s failglob
+
+rm -rf manifests/base
+mkdir -p manifests/base
+pushd manifests/base > /dev/null || exit 1
+
+export NAMESPACE="netbox"
+
+# https://artifacthub.io/packages/helm/netbox/netbox
+# The startupProbe.failureThreshold is super high because it initializes the database on first run and takes ages
+helm template netbox oci://ghcr.io/netbox-community/netbox-chart/netbox \
+    --namespace "${NAMESPACE}" \
+    --include-crds \
+    --set 'cachingRedis.existingSecretKey=redis-password' \
+    --set 'cachingRedis.existingSecretName=redis-password' \
+    --set 'existingSecret=netbox-config' \
+    --set 'externalDatabase.existingSecretKey=password' \
+    --set 'externalDatabase.existingSecretName=netbox-postgresql-credentials' \
+    --set 'externalDatabase.host=netbox-postgresql-ha.netbox.svc' \
+    --set 'externalDatabase.user=netbox' \
+    --set 'ingress.className=cilium' \
+    --set 'ingress.enabled=true' \
+    --set 'ingress.hosts[0].host=netbox.fh.dronen.house' \
+    --set 'ingress.hosts[0].paths[0]="/"' \
+    --set 'ingress.tls[0].hosts[0]=netbox.fh.dronen.house' \
+    --set 'ingress.tls[0].secretName=netbox-server-tls' \
+    --set 'postgresql.enabled=false' \
+    --set 'redis.enabled=false' \
+    --set 'startupProbe.failureThreshold=60' \
+    --set 'superuser.existingSecret=netbox-superuser' \
+    --set 'tasksRedis.existingSecretKey=redis-password' \
+    --set 'tasksRedis.existingSecretName=redis-password' | \
+    yq --no-colors --prettyPrint '... comments=""' | \
+    kubectl-slice -o . --template "{{ .kind | lower }}.yaml"
+
+# Remove the test pod
+rm pod.yaml
+
+echo "---" > namespace.yaml
+kubectl create namespace "${NAMESPACE}" -o yaml --dry-run=client | \
+    kubectl neat \
+    >> namespace.yaml
+
+kustomize create --autodetect
+
+# Format YAML
+prettier . --write
+popd > /dev/null || exit 1
